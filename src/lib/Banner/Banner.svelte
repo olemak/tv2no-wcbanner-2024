430d71ca2ae1e076c85aca6d59b9403ed46d3408
Ole Martin Kristiansen <OleMartin.Kristiansen@tv2.no>
2024-11-11 14:24:43 +0100

    First steps

---
+++ src/lib/Banner/Banner.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    let { current } = $props<{current:string}>();
+</script>
+
+<section class="banner">
+    <h2>This is the banner</h2>
+    <h3>Change the markup</h3>
+    <p>Thingnes Bø: {current}</p>
+    <p>Bjørndalen: 95</p>
+</section>
+
+<style>
+
+    .banner {
+        width: 100%;
+        border: 2px solid #000;
+        padding: 1rem;
+        background: blue;
+        color: white;
+        display: flex;
+        max-width: 768px;
+        justify-content: space-between;
+        align-items: center;
+        margin: 5rem auto;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
-<h1>Welcome to your library project</h1>
+<script>
+  import Banner from "$lib/Banner/Banner.svelte";
+</script>
+
+<h1>Skiskyting banner:</h1>
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<Banner current="84" />
